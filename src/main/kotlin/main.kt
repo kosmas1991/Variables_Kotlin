0e8bf9637637d37c8f213fd2258b6912fa0c9605
kosmas1991 <kosmasgou@gmail.com>
2021-01-14 18:26:04 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var name = "kosmas"
+    println(name)
+    var age = 14
+    print(age)
+    print("\n")
+    var lastName : String?
+    lastName = "gou"
+    println(lastName)
+
+}
